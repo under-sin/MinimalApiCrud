a02e86620626d16b45d66a422fbeeed71e982622
Andy <andersonvieira818@yahoo.com.br>
2023-12-20 09:04:56 -0300

    Add insert, update and delete methods in CarRepository

--- Interfaces/ICarRepository.cs
+++ Interfaces/ICarRepository.cs
@@ -1,8 +1,11 @@
-﻿using MinimalApiCrud.Models;
+﻿using MinimalApiCrud.Model;
 
 namespace MinimalApiCrud.Interfaces;
 
 public interface ICarRepository
 {
-    IEnumerable<CarModels> GetCars();
+    IEnumerable<CarModel> GetCars();
+    bool InsertCar(CarModel car);
+    bool UpdateCarCor(int id, string cor);
+    bool Delete(int id);
 }
--- Repositories/CarRepository.cs
+++ Repositories/CarRepository.cs
@@ -2,7 +2,7 @@
 using Dapper;
 using MinimalApiCrud.Factory;
 using MinimalApiCrud.Interfaces;
-using MinimalApiCrud.Models;
+using MinimalApiCrud.Model;
 
 namespace MinimalApiCrud.Repositories;
 
@@ -14,16 +14,77 @@ public class CarRepository : ICarRepository
         _connection = new SqlFactory().SqlConnection();
     }
 
-    public IEnumerable<CarModels> GetCars()
+    public IEnumerable<CarModel> GetCars()
     {
-        var cars = new List<CarModels>();
+        var cars = new List<CarModel>();
         var query = "SELECT * FROM [CarDataBase].[dbo].[Cars]";
 
         using (_connection)
         {
-            cars = _connection.Query<CarModels>(query).ToList();
+            cars = _connection.Query<CarModel>(query).ToList();
         }
 
         return cars;
+    }
+
+    public bool InsertCar(CarModel car)
+    {
+        var query = @"INSERT INTO [CarDataBase].[dbo].[Cars]
+        VALUES
+        (
+            @modelo,
+            @fabricante,
+            @motor,
+            @cor
+        )";
+        var parameters = new
+        {
+            modelo = car.Modelo,
+            fabrincante = car.Fabricante,
+            motor = car.Motor,
+            cor = car.Cor
+        };
+        var result = 0;
+
+        using (_connection)
+        {
+            result = _connection.Execute(query, parameters);
+        }
+
+        return (result != 0 ? true : false);
+    }
+
+    public bool UpdateCarCor(int id, string cor)
+    {
+        var query = @"UPDATE [CarDataBase].[dbo].[Cars]
+        SET [Cor] = @corCarro
+        WHERE [Id] = @carId";
+        var parameters = new
+        {
+            carId = id,
+            corCarro = cor
+        };
+        var result = 0;
+
+        using (_connection)
+        {
+            result = _connection.Execute(query, parameters);
+        }
+
+        return (result != 0 ? true : false);
+    }
+
+    public bool Delete(int id)
+    {
+        var query = "DELETE [CarDataBase].[dbo].[Cars] WHERE [Id] = @carId";
+        var parameters = new { carId = id };
+        int result = 0;
+
+        using (_connection)
+        {
+            result = _connection.Execute(query, parameters);
+        }
+
+        return (result != 0 ? true : false);
     }
 }
