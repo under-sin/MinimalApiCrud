4dcffd56a5aee125ecb0bd19c44f5da59758d542
Andy <andersonvieira818@yahoo.com.br>
2023-12-20 09:04:09 -0300

    Change model name

---
+++ Model/CarModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalApiCrud.Model;
+
+public class CarModel
+{
+    public int Id { get; set; }
+    public string? Modelo { get; set; }
+    public string? Fabricante { get; set; }
+    public string? Motor { get; set; }
+    public string? Cor { get; set; }
+}
